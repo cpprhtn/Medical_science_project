35fd9119b3d219ed49eeb4b338718a5e72a9ff8a
Lee_JunWon <cpprhtn@naver.com>
2020-07-08 22:15:25 +0900

    7/8

--- Crawling Model.py
+++ Crawling Model.py
@@ -68,21 +68,8 @@ pyautogui.moveTo(600,572,3)
 #왼쪽 버튼 클릭
 pyautogui.click()
 
-#검색 입력 부분에 커서를 올리고
-#검색 입력 부분에 다양한 명령을 내리기 위해 elem 변수에 할당한다
-elem = driver.find_element_by_name("id")
- 
-#입력 부분에 default로 값이 있을 수 있어 비운다
-elem.clear()
- 
-#검색어를 입력한다
-elem.send_keys("Selenium")
- 
-#검색을 실행한다
-elem.submit()
- 
-#검색이 제대로 됐는지 확인한다
-assert "No results found." not in driver.page_source
+
+
  
 #브라우저를 종료한다
 driver.close()
