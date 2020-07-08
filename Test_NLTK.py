365327f5d0528bf12ec21b9c6e00e6f8afbeff2e
Lee_JunWon <cpprhtn@naver.com>
2020-07-08 21:31:24 +0900

    7/8

--- Test_NLTK.py
+++ Test_NLTK.py
@@ -14,7 +14,7 @@ nltk.download('punkt')
 import nltk
 nltk.download('stopwords')
 
-example = "Family is not an important thing. It's everything."
+example = "The South African response in dealing with the Corona pandemic needs to speak to the realities of <i>all</i> people living in the country, including migrant and refugee communities. Reflecting on this in light of ongoing research on the political stakes of migration governance, we find that the virus response shows little change in the government agenda when it comes to dealing with refugees and other migrants. Veritably, we see that the pandemic may even be an excuse for pushing through already-aspired to policies. This includes the securitised agenda behind the sudden building of a border fence to close off Zimbabwe and the xenophobic-rhetorical clout behind the lockdown rules about which shops are allowed to remain open. The temporary stay on renewing asylum seekers permits counts as a perfunctory exception. We show that each of these developments very much play into politics as usual."
 stop_words = set(stopwords.words('english')) 
 
 word_tokens = word_tokenize(example)
